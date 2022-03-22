4ae54383c051e9674be593056152447f834dfb89
Kar Technologies (A Unit of Kar Group of Companies) <95956303+karllc@users.noreply.github.com>
2022-03-22 11:37:43 +0530

    Updated

--- software/demos/js/main.js
+++ software/demos/js/main.js
@@ -1,63 +1,60 @@
-(function () {
+(function() {
     'use strict';
-    
+
     // Preloader
-	$("#preloader").fadeOut(900);
-	$(".preloader-bg").delay(800).fadeOut(900);
-	var wind = $(window);
-    
-    
+    $("#preloader").fadeOut(900);
+    $(".preloader-bg").delay(800).fadeOut(900);
+    var wind = $(window);
+
+
     var isMobile = {
-        Android: function () {
+        Android: function() {
             return navigator.userAgent.match(/Android/i);
-        }
-        , BlackBerry: function () {
+        },
+        BlackBerry: function() {
             return navigator.userAgent.match(/BlackBerry/i);
-        }
-        , iOS: function () {
+        },
+        iOS: function() {
             return navigator.userAgent.match(/iPhone|iPad|iPod/i);
-        }
-        , Opera: function () {
+        },
+        Opera: function() {
             return navigator.userAgent.match(/Opera Mini/i);
-        }
-        , Windows: function () {
+        },
+        Windows: function() {
             return navigator.userAgent.match(/IEMobile/i);
-        }
-        , any: function () {
+        },
+        any: function() {
             return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());
         }
     };
     // Full Height
-    var fullHeight = function () {
+    var fullHeight = function() {
         if (!isMobile.any()) {
             $('.js-fullheight').css('height', $(window).height());
-            $(window).resize(function () {
+            $(window).resize(function() {
                 $('.js-fullheight').css('height', $(window).height());
             });
         }
     };
     // Animations
-    var contentWayPoint = function () {
+    var contentWayPoint = function() {
         var i = 0;
-        $('.animate-box').waypoint(function (direction) {
+        $('.animate-box').waypoint(function(direction) {
             if (direction === 'down' && !$(this.element).hasClass('animated')) {
                 i++;
                 $(this.element).addClass('item-animate');
-                setTimeout(function () {
-                    $('body .animate-box.item-animate').each(function (k) {
+                setTimeout(function() {
+                    $('body .animate-box.item-animate').each(function(k) {
                         var el = $(this);
-                        setTimeout(function () {
+                        setTimeout(function() {
                             var effect = el.data('animate-effect');
                             if (effect === 'fadeIn') {
                                 el.addClass('fadeIn animated');
-                            }
-                            else if (effect === 'fadeInLeft') {
+                            } else if (effect === 'fadeInLeft') {
                                 el.addClass('fadeInLeft animated');
-                            }
-                            else if (effect === 'fadeInRight') {
+                            } else if (effect === 'fadeInRight') {
                                 el.addClass('fadeInRight animated');
-                            }
-                            else {
+                            } else {
                                 el.addClass('fadeInUp animated');
                             }
                             el.removeClass('item-animate');
@@ -70,23 +67,22 @@
         });
     };
     // Burger Menu 
-    var burgerMenu = function () {
-        $('.js-lonon-nav-toggle').on('click', function (event) {
+    var burgerMenu = function() {
+        $('.js-lonon-nav-toggle').on('click', function(event) {
             event.preventDefault();
             var $this = $(this);
             if ($('body').hasClass('offcanvas')) {
                 $this.removeClass('active');
                 $('body').removeClass('offcanvas');
-            }
-            else {
+            } else {
                 $this.addClass('active');
                 $('body').addClass('offcanvas');
             }
         });
     };
     // Mobile Menu outside of offcanvass
-    var mobileMenuOutsideClick = function () {
-        $(document).on(function (e) {
+    var mobileMenuOutsideClick = function() {
+        $(document).on(function(e) {
             var container = $("#lonon-aside, .js-lonon-nav-toggle");
             if (!container.is(e.target) && container.has(e.target).length === 0) {
                 if ($('body').hasClass('offcanvas')) {
@@ -95,7 +91,7 @@
                 }
             }
         });
-        $(window).scroll(function () {
+        $(window).scroll(function() {
             if ($('body').hasClass('offcanvas')) {
                 $('body').removeClass('offcanvas');
                 $('.js-lonon-nav-toggle').removeClass('active');
@@ -103,23 +99,21 @@
         });
     };
     // Sticky 
-    var stickyFunction = function () {
+    var stickyFunction = function() {
         var h = $('.image-content').outerHeight();
         if ($(window).width() <= 992) {
             $("#sticky_item").trigger("sticky_kit:detach");
-        }
-        else {
+        } else {
             $('.sticky-parent').removeClass('stick-detach');
             $("#sticky_item").trigger("sticky_kit:detach");
             $("#sticky_item").trigger("sticky_kit:unstick");
         }
-        $(window).resize(function () {
+        $(window).resize(function() {
             var h = $('.image-content').outerHeight();
             $('.sticky-parent').css('height', h);
             if ($(window).width() <= 992) {
                 $("#sticky_item").trigger("sticky_kit:detach");
-            }
-            else {
+            } else {
                 $('.sticky-parent').removeClass('stick-detach');
                 $("#sticky_item").trigger("sticky_kit:detach");
                 $("#sticky_item").trigger("sticky_kit:unstick");
@@ -129,16 +123,16 @@
         $('.sticky-parent').css('height', h);
         $("#sticky_item").stick_in_parent();
     };
-    
+
     // Progress-wrap
-     var progressPath = document.querySelector('.progress-wrap path');
+    var progressPath = document.querySelector('.progress-wrap path');
     var pathLength = progressPath.getTotalLength();
     progressPath.style.transition = progressPath.style.WebkitTransition = 'none';
     progressPath.style.strokeDasharray = pathLength + ' ' + pathLength;
     progressPath.style.strokeDashoffset = pathLength;
     progressPath.getBoundingClientRect();
     progressPath.style.transition = progressPath.style.WebkitTransition = 'stroke-dashoffset 10ms linear';
-    var updateProgress = function () {
+    var updateProgress = function() {
         var scroll = $(window).scrollTop();
         var height = $(document).height() - $(window).height();
         var progress = pathLength - (scroll * pathLength / height);
@@ -148,33 +142,35 @@
     $(window).scroll(updateProgress);
     var offset = 150;
     var duration = 550;
-    jQuery(window).on('scroll', function () {
+    jQuery(window).on('scroll', function() {
         if (jQuery(this).scrollTop() > offset) {
             jQuery('.progress-wrap').addClass('active-progress');
         } else {
             jQuery('.progress-wrap').removeClass('active-progress');
         }
     });
-    jQuery('.progress-wrap').on('click', function (event) {
+    jQuery('.progress-wrap').on('click', function(event) {
         event.preventDefault();
-        jQuery('html, body').animate({ scrollTop: 0 }, duration);
+        jQuery('html, body').animate({
+            scrollTop: 0
+        }, duration);
         return false;
     })
-    
+
     // Button
     var buttons = document.querySelectorAll(".btn .blog-entry .blog-img ol.comment-list li.comment .reply a");
-    for(var i = 0; i < buttons.length; i++) {
-      var button = buttons[i];
-      button.addEventListener("click", function() {
-        if(!button.classList.contains("active"))
-          button.classList.add("active");
-        else
-          button.classList.remove("active");
-      });
+    for (var i = 0; i < buttons.length; i++) {
+        var button = buttons[i];
+        button.addEventListener("click", function() {
+            if (!button.classList.contains("active"))
+                button.classList.add("active");
+            else
+                button.classList.remove("active");
+        });
     }
-    
+
     // Document on load.
-    $(function () {
+    $(function() {
         fullHeight();
         contentWayPoint();
         burgerMenu();
