93bd12d47476530d6ec170f8e68b260d0d980cb9
Kar Technologies (A Unit of Kar Group of Companies) <95956303+karllc@users.noreply.github.com>
2022-05-04 11:21:46 +0530

    Updated

---
+++ software/demos/demo/html/phoxel/js/main.js
@@ -0,0 +1,10 @@
+(function ($) {
+    
+    "use strict";
+    
+    // Preloader
+	$("#preloader").fadeOut(900);
+	$(".preloader-bg").delay(800).fadeOut(900);
+	var wind = $(window); 
+    
+})(jQuery);
